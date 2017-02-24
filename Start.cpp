e2a229360958838c43652590c6c5686834920245
takuya <test>
2017-02-24 22:30:59 +0900

    update:状態管理を作成

---
+++ Start.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file Start.h
+ * @brief 起動
+ * @author tkhs
+ * @date 2017 02 24 Friday
+ */
+#include <iostream>
+#include "StatusSubscriber.h"
+#include "Start.h"
+
+void Start::StartTask()
+{
+  StatusSubscriber status_subscriber = StatusSubscriber::getInstance();
+
+
+
+}
